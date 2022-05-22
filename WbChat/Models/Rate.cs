a98ca992a4ff09bbec4dcd9d7d80642915df41bd
hodamarr <104125115+hodamarr@users.noreply.github.com>
2022-05-22 18:22:41 +0300

    Add files via upload

---
+++ WbChat/Models/Rate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace web_chat.Models
+{
+    public class Rate
+    {
+       public int ID { get; set; }
+       public string Name { get; set; }
+       public int Score { get; set; }
+       public string Content { get; set; }
+    }
+}
